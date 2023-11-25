e7ee25dbf84b2661ec747b119c173a669f09297d
Mythbeast <joel_ab@hotmail.co.uk>
2023-11-25 14:54:41 +0000

    Lap counter and progress added

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,17 +11,26 @@ var acceleration = Vector2(0, 0)
 var rotation_input = 0
 var accel_input = 0
 var direction_change = 0
+var racetrack: Node2D
+var lap_progress:float 
+var previous_progress:float 
+var lap_count = 0
+
 # collision_layer and collision_mask defaults are 1
 
 @onready var accel_particles: GPUParticles2D = get_node("AccelerationParticles")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = Vector2(INITIAL_SPEED * cos(deg_to_rad(rotation)), INITIAL_SPEED * sin(deg_to_rad(rotation)))
+		velocity = Vector2(INITIAL_SPEED * cos(deg_to_rad(rotation)), INITIAL_SPEED * sin(deg_to_rad(rotation)))
 
 @warning_ignore("unused_parameter")
 func _process(delta):
-	pass
+	accel_particles.emitting = accel_input > 0
+	lap_progress = racetrack.get_lap_progress(position)
+	if previous_progress - lap_progress > 0.9:
+		lap_count += 1
+	print(lap_count)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -35,7 +44,7 @@ func _physics_process(delta):
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
 
-	accel_particles.emitting = accel_input > 0
+	
 	
 
 # Receives input from the player via arrow keys
