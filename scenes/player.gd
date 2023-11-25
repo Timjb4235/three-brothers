320f7922e53da0d63224d9c34d9ce7a80d0da6ed
timjb4235 <timbryce4235@gmail.com>
2023-11-25 10:10:42 +0000

    player movement updated

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-# Direction is clockwise from right
 const initial_speed = 50
+const friction = 0.99
+const accel_strength = 5
 var acceleration = Vector2(0, 0)
 var rotation_input = 0
 var accel_input = 0
 var direction_change = 0
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,13 +18,14 @@ func _process(delta):
 	get_input()
 	rotation += rotation_input * delta
 	print(transform.x)
+	velocity *= friction
 	velocity += transform.x * accel_input
 	move_and_collide(velocity * delta)
 	
 # Receives input from the player via arrow keys
 func get_input():
 	rotation_input = Input.get_axis("left", "right")
-	accel_input = 5 * Input.get_axis("down", "up")
+	accel_input = accel_strength * Input.get_axis("down", "up")
 	print(accel_input)
 	print(rotation_input)
 
