61b554e22fd28ffc12860655379ead6fd56d21dc
timjb4235 <timbryce4235@gmail.com>
2023-11-25 10:00:07 +0000

    Acceleration successful

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,38 +1,30 @@
 extends CharacterBody2D
 
 # Direction is clockwise from right
-const initial_speed = 100
+const initial_speed = 50
 var acceleration = Vector2(0, 0)
-var angle = 0
-var rotation_input
-var accel_input
+var rotation_input = 0
+var accel_input = 0
 var direction_change = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = Vector2(initial_speed * cos(deg_to_rad(angle)), initial_speed * sin(deg_to_rad(angle)))
+	velocity = Vector2(initial_speed * cos(deg_to_rad(rotation)), initial_speed * sin(deg_to_rad(rotation)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_input()
-	#print(accel_input)
-	#print(rotation_input)
-	angle += rotation_input
+	rotation += rotation_input * delta
 	print(transform.x)
 	velocity += transform.x * accel_input
-	#print(accel_input)
-	#print(velocity)
 	move_and_collide(velocity * delta)
-	# speed += acceleration
-	# angle += direction_change
-	# velocity = Vector2(speed * cos(deg_to_rad(angle)), speed * sin(deg_to_rad(angle)))
-
+	
 # Receives input from the player via arrow keys
 func get_input():
-	# Left negative, right positive
 	rotation_input = Input.get_axis("left", "right")
-	accel_input = Input.get_axis("down", "up")
+	accel_input = 5 * Input.get_axis("down", "up")
 	print(accel_input)
 	print(rotation_input)
+
