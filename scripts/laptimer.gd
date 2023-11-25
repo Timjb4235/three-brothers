8ce712c037fd92afd8b5725732d51be2abb505bf
Dan Quill <danbryce1@gmail.com>
2023-11-25 17:05:45 +0000

    Improve fonts; fix player collider

--- scripts/laptimer.gd
+++ scripts/laptimer.gd
@@ -4,18 +4,12 @@ var minutes = 0.0
 var seconds = 0.0
 var milliseconds = 0.0
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	gametime += delta
 	minutes = gametime / 60
 	seconds = fmod(gametime,60)
 	milliseconds = fmod(gametime,1)*100
-	text = str("%02d,%02d,%02d") % [minutes,seconds,milliseconds] 
-	
-
+	text = str("%02d:%02d:%02d") % [minutes,seconds,milliseconds] 
